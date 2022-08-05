0a32e6298a04a1228d2cb9e99548455f79aa4619
Aleksandra Opacic <aleks.opacic@hotmail.com>
2022-08-05 16:25:46 +0100

    centering

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import './styles.css'
 
 const CheckIn = () =>{
     return <>
+    <div className='container'>
         <h1 role="h1">This is the about page</h1>
+        </div>
     </>
 }
 
