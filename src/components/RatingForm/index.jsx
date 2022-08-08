850b563274583b02aa54181207ad287f60c62125
Aaron Smith <2021manifest@gmail.com>
2022-08-08 11:40:42 +0100

    checkIn page, changed radio input to range

--- src/components/RatingForm/index.jsx
+++ src/components/RatingForm/index.jsx
@@ -5,46 +5,15 @@ const RatingForm = ({ label, funct }) => {
     <div className="rating-q-container">
       <div className="rating-q">
         <h2>{label}: </h2>
+
         <input
-          type="radio"
-          value="1"
-          name={label}
           onChange={funct}
-          id="radio-input"
+          type="range"
+          defaultValue="1"
+          min="0"
+          max="5"
+          oninput="rangeValue.innerText = this.value"
         />
-        <label htmlFor="">1</label>
-        <input
-          type="radio"
-          value="2"
-          onChange={funct}
-          name={label}
-          id="radio-input"
-        />
-        <label htmlFor="">2</label>
-        <input
-          type="radio"
-          value="3"
-          onChange={funct}
-          name={label}
-          id="radio-input"
-        />
-        <label htmlFor="">3</label>
-        <input
-          type="radio"
-          value="4"
-          onChange={funct}
-          name={label}
-          id="radio-input"
-        />
-        <label htmlFor="">4</label>
-        <input
-          type="radio"
-          value="5"
-          onChange={funct}
-          name={label}
-          id={label}
-        />
-        <label htmlFor="">5</label>
       </div>
     </div>
   );
