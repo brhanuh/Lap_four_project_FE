53f4b32ba671a8b9221649297847bf9e972d6d79
Aleksandra Opacic <aleks.opacic@hotmail.com>
2022-08-08 12:01:02 +0100

    added random quotes generator

--- src/components/Quotes/index.jsx
+++ src/components/Quotes/index.jsx
@@ -1,18 +1,37 @@
 import React, {useState, useEffect} from 'react'
+import axios from "axios";
 
 const Quotes = () =>{
 
 const [quote, setQuote] = useState("");
 const [author, setAuthor] = useState("");
 
+const quoteAPI= async () => {
+    let arrayOfQuotes = [];
+    try{
+        const data = await axios.get("https://api.quotable.io/random");
+        arrayOfQuotes = data.data
+    }catch (error){
+        console.log(error);
+    }
+
+    try{
+        setQuote(arrayOfQuotes.content)
+        setAuthor(arrayOfQuotes.author)
+    }catch (error){
+        console.log(error);
+    }
+};
+
+useEffect(() => {
+    quoteAPI();
+},[]);
 
     return <>
     <div className='quote'>
-        <h2>New Quote:</h2>
-        <button>Generate new quote</button>
-
+        {quote}
+        {author}
     </div>
-    
     </>
 }
 
