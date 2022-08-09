6b3e43b1c88747397195e5d7085450f31a95354a
brhanuh <hanibalbrhanu15@hotmail.com>
2022-08-09 08:36:30 +0100

    commented route protection

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,34 +1,33 @@
-import React, { useState } from 'react';
-import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
-import { NavLink, Outlet } from 'react-router-dom';
-import { SidebarData } from './SidebarData';
-import './layout.css'
-import '../index.css'
-import MoodMode from '../components/MoodMode';
-import MoodBoard from '../components/MoodBoard';
+import React, { useState } from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import { NavLink, Outlet } from "react-router-dom";
+import { SidebarData } from "./SidebarData";
+import "./layout.css";
+import "../index.css";
+import MoodMode from "../components/MoodMode";
+import MoodBoard from "../components/MoodBoard";
 import { Login } from "../pages";
-
 
 function Navbar() {
   const [sidebar, setSidebar] = useState(false);
 
   const showSidebar = () => setSidebar(!sidebar);
 
-  const token = localStorage.getItem("token");
-  if (!token) {
-    return <Login />;
-  }
+  // const token = localStorage.getItem("token");
+  // if (!token) {
+  //   return <Login />;
+  // }
   return (
     <>
-        <div className='navbar'>
-          <NavLink to='#' className='menu-bars'>
-            <FaIcons.FaBars onClick={showSidebar} />
-          </NavLink>
-          <MoodBoard/>
-          <div className='container'>
+      <div className="navbar">
+        <NavLink to="#" className="menu-bars">
+          <FaIcons.FaBars onClick={showSidebar} />
+        </NavLink>
+        <MoodBoard />
+        <div className="container">
           <h1>Mental Health app</h1>
-          </div>
+        </div>
         <MoodMode />
       </div>
       <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
