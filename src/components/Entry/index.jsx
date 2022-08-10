77d86f2636808651b5a8ff3753fc3131236e5518
brhanuh <hanibalbrhanu15@hotmail.com>
2022-08-10 14:41:58 +0100

    user timeline and view entries

--- src/components/Entry/index.jsx
+++ src/components/Entry/index.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-const Entry = ({ year, text, cssClass, index }) => {
+const Entry = ({ year, text, cssClass, index, entry }) => {
   let navigate = useNavigate();
-  console.log(cssClass);
-  console.log(index);
   function handleView(e) {
     e.preventDefault();
-    console.log("hello world");
-    console.log(index);
-    navigate("/timeline/entry", { state: { index: index } });
+    navigate("/timeline/entry", { state: { index: index, entry: entry } });
     //use navigate to view more about the entry, use timeline
   }
   return (
--- src/pages/ViewEntry/index.jsx
+++ src/pages/ViewEntry/index.jsx
@@ -1,14 +1,24 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import "./viewEntry.css";
 const ViewEntry = ({ data }) => {
   const location = useLocation();
-  console.log("location", location);
+  //console.log("location", location.state.entry.user);
   return (
     <>
-      <div>
-        <div className="content">
-          <h2>Hello World</h2>
-          <p>I am now here</p>
+      <div className="viewEntryBody">
+        <div className="contents">
+          <h2>Entry details</h2>
+          <p>Energy: {location.state.entry.energy}</p>
+          <p>Depression: {location.state.entry.depression}</p>
+          <p>Irritability: {location.state.entry.irritability}</p>
+          <p>Motivation: {location.state.entry.motivation}</p>
+          <p>Stress: {location.state.entry.stress}</p>
+          <p>Appetite: {location.state.entry.appetite}</p>
+          <p>Concentratio: {location.state.entry.concentration}</p>
+          <p>Diet: {location.state.entry.diet}</p>
+          <p>Entertainment: {location.state.entry.entertainment}</p>
+          <p>Social: {location.state.entry.social}</p>
         </div>
       </div>
     </>
