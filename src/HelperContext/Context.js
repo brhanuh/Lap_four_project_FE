4ab9f7faf395675cedc7de809c10b25e045d6f2b
Aaron Smith <2021manifest@gmail.com>
2022-08-12 09:57:21 +0100

    added

--- src/HelperContext/Context.js
+++ src/HelperContext/Context.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useRef, useState } from "react";
 
 const AppContext = createContext()
 
@@ -9,11 +9,27 @@ export function AppProvider({ children }) {
     const [textAreaValue, setTextValue] = useState('');
     const [class1, setClass] = useState('');
     const [userFeelObj, setUserFeelObj] = useState();
+    const [show, setShow] = useState(false);
+    const [formData, setFormData] = useState({
+        energy: 0,
+        depression: 0,
+        irritability: 0,
+        motivation: 0,
+        stress: 0,
+        appetite: 0,
+        concentration: 0,
+        diet: '',
+        enter: '',
+        social: '',
+        mood: 0,
+    });
+    const emojiRef = useRef();
+
 
 
 
     return (
-        <AppContext.Provider value={{ userFeelObj, setUserFeelObj, userValueData, feelingValue, setFeelingValue, brainColor, setBrainColor, feeling, setFeeling, textAreaValue, setTextValue, class1, setClass }}>
+        <AppContext.Provider value={{ emojiRef, formData, setFormData, show, setShow, userFeelObj, setUserFeelObj, feelingValue, setFeelingValue, brainColor, setBrainColor, feeling, setFeeling, textAreaValue, setTextValue, class1, setClass }}>
             {children}
         </AppContext.Provider>
     )
